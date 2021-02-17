7b9284ac1b4bc59ffad5347198a12f94e4870ce7
lhsthree <lhsthree@gmail.com>
2021-02-17 12:17:05 -0600

    feat: add context provider and use for modal and sidebar

---
+++ src/context.js
@@ -0,0 +1,43 @@
+import React, {useState, useContext } from 'react'
+
+
+const AppContext = React.createContext();
+
+const AppProvider = ({children}) => {
+const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+const [isModalOpen, setIsModalOpen] = useState(false)
+
+const openSidebar = () => {
+	setIsSidebarOpen(true)
+}
+
+const openModal = () => {
+	setIsModalOpen(true)
+}
+
+const closeSidebar = () => {
+	setIsSidebarOpen(false)
+}
+
+const closeModal = () => {
+	setIsModalOpen(false)
+}
+
+
+	return (
+			<AppContext.Provider 
+				value={{
+					isModalOpen, 
+					isSidebarOpen, 
+					openModal, 
+					closeModal, 
+					openSidebar, 
+					closeSidebar
+				}}>{children}</AppContext.Provider>)
+}
+
+export const useGlobalContext = () => {
+	return useContext(AppContext)
+}
+
+export {AppContext, AppProvider}
